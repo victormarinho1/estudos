f362ad3a4a375b2c3251daebddbfd4920e22b482
VictorVFM <victorhugoferreira452@gmail.com>
2025-07-10 20:13:41 -0300

    Commit

--- php/uec/index.php
+++ php/uec/index.php
@@ -29,10 +29,18 @@
     "Austrália", 28, 1.93,
     81.6, 13,0,2);
 
-    $l[0]->status();
-    $l[1]->status();
-    $l[2]->status();
-    $l[3]->status();
+    $l[4] = new Lutador("OFOCobol",
+    "Brasil", 37, 1.70,
+    119.3, 5,4,3);
+
+    $l[5] = new Lutador("Dead Code",
+    "Austrália", 30, 1.81,
+    105.7, 12,2,4);
+
+    
+    $l[3]->perderLuta();
+    $l[3]->perderLuta();
+    $l[3]->apresentar();
     
     ?>
 
